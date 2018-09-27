53c4b20b18308ad496bf86ac3ef861c6b49038e0
Abhijit Parida <abhijitparida.me@gmail.com>
2018-09-27 15:00:17 +0530

    Add show/hide password buttons

--- app/src/main/java/app/passworder/PasswordListActivity.java
+++ app/src/main/java/app/passworder/PasswordListActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -59,10 +60,10 @@ public class PasswordListActivity extends AppCompatActivity {
             LayoutInflater layoutInflater = LayoutInflater.from(PasswordListActivity.this);
             convertView = layoutInflater.inflate(R.layout.item_password, parent, false);
             TextView website = convertView.findViewById(R.id.website);
-            TextView password = convertView.findViewById(R.id.password);
+            final TextView password = convertView.findViewById(R.id.password);
             final Password p = passwords.get(position);
             website.setText(p.getWebsite());
-            password.setText("Password: " + p.generatePassword(masterPassword));
+            password.setText("Password: ************");
 
             Button copyPassword = convertView.findViewById(R.id.copyPassword);
             copyPassword.setOnClickListener(new View.OnClickListener() {
@@ -72,6 +73,25 @@ public class PasswordListActivity extends AppCompatActivity {
                     ClipData clip = ClipData.newPlainText("password", p.generatePassword(masterPassword));
                     clipboard.setPrimaryClip(clip);
                     Toast.makeText(PasswordListActivity.this, "Password copied", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            final ImageView showPassword = convertView.findViewById(R.id.showPassword);
+            final ImageView hidePassword = convertView.findViewById(R.id.hidePassword);
+            showPassword.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showPassword.setVisibility(View.GONE);
+                    hidePassword.setVisibility(View.VISIBLE);
+                    password.setText("Password: " + p.generatePassword(masterPassword));
+                }
+            });
+            hidePassword.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    hidePassword.setVisibility(View.GONE);
+                    showPassword.setVisibility(View.VISIBLE);
+                    password.setText("Password: ************");
                 }
             });
 
